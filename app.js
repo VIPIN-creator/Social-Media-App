2f7cb616492f0ed86234a2940b7b68a02c8f8e3a
Vipin <vipintripathi121@gmail.com>
2021-08-03 02:21:14 +0530

    add env

--- app.js
+++ app.js
@@ -3,7 +3,23 @@ const path = require('path');
 
 const app = express();
 
+//  initiate dotenv and make your environment variables available throughout your application
+const dotnev = require('dotenv');
+dotnev.config();
 const PORT = process.env.PORT || 4000;
+
+const db = require('./db');
+
+// connect to db
+db.connect(`${DATABASEURL}`, function(err){
+    if(err){
+        console.log('Unable to connect');
+        process.exit(1);
+    }
+    else{
+        app.listen(PORT, () => console.log(`Server is running on ${PORT}`) );
+    }
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, '/views'));
@@ -11,4 +27,3 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res)  => {res.render('home-guest')});
 
-app.listen(PORT, () => console.log(`Server is running on ${PORT}`) );
